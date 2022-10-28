71a2ba8c546b07e573c744cdd098e8080c388c58
MTC00747 <matheustcavalcanti@hotmail.com>
2022-10-28 07:28:40 -0300

    Atividade ER3

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -9,31 +9,43 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private  userService : UserService) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
   //Classes e métodos
-  userModel= new User() 
-  Mensagem ="";
+  userModel = new User()
+  Mensagem = "";
   receberDados() {
-    console.log(this.userModel);
-    //Disparando a funcionaldiade logar usuario 
-    this.userService.logarUsuario(this.userModel).subscribe({
-      next: (response) => {
-        console.log("Deu certo!")
-        console.log(response)
-        this.Mensagem="Logado com sucesso";
-      },
-      error: (err) => {
-        console.log("Deu Erro!")
-        console.log(err)
-        this.Mensagem= err.error;
-      },
-      
-    
-    })
-    
-  }
+    // console.log(this.userModel);
+    const Blaclist = ["SELECT", "OR", '"="', "--", ";", "1 = 1", "1=1", "DROP", "\"\"=\"\"", "'='"];
+    let Ataque = 0
+    //Percorrer o arrray e verificar se encontrou alguma palavra da blacklist
+    Blaclist.forEach((Palavra) => {
+      if (this.userModel.email?.toUpperCase().includes(Palavra)) {
+        Ataque++
+      }
+    });
 
+    if (this.userModel.email == "" || this.userModel.password == "" || Ataque > 0) {//palavra proibida ou se esqueeu de preencher algum campo 
+      this.Mensagem = "Preencher os campos corretamente";
+    } else {
+      //Disparando a funcionaldiade logar usuario 
+      this.userService.logarUsuario(this.userModel).subscribe({
+        next: (response) => {
+          console.log("Deu certo!")
+          console.log(response)
+          this.Mensagem = "Logado com sucesso";
+        },
+        error: (err) => {
+          console.log("Deu Erro!")
+          console.log(err)
+          this.Mensagem = err.error;
+        },
+
+
+      })
+    }
+
+  }
 }
